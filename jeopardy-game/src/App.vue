e3221dd785c72b571c12727d210cd4fae0bb1956
Ryan Gaudier <113002432+RyanGudi@users.noreply.github.com>
2024-11-01 17:03:00 -0700

    Player Cards Created

--- jeopardy-game/src/App.vue
+++ jeopardy-game/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PlayerInfo></PlayerInfo>
+  <GameBoard></GameBoard>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlayerInfo from './components/PlayerInfo.vue';
+//import GameBoard from './components/GameBoard.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PlayerInfo,
+    //GameBoard
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* #app {
+ 
+} */
 </style>
---
+++ jeopardy-game/src/components/PlayerInfo.vue
@@ -0,0 +1,49 @@
+<template>
+    <h1>Jeopardy</h1>
+    <div class="players-container">
+        <div class="playerCard">
+            <p>Player 1</p>
+            <h2>Score</h2>
+        </div>
+        <div class="playerCard">
+            <p>Player 2</p>
+            <h2>Score</h2>
+        </div>
+        <div class="playerCard">
+            <p>Player 3</p>
+            <h2>Score</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+
+    .players-container {
+        display: flex;
+        justify-content: center;
+        gap: 3rem;
+    }
+
+    .players-container h2 {
+        padding-right: 3rem;
+    }
+
+    .playerCard {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: #daa453;
+        background-color: #0f197b;
+        width: 20%;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.5);
+        border-radius: 15px;
+        padding: 1rem;
+    }
+</style>
