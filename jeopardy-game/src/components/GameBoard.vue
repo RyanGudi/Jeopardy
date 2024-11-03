371b1f47baf1a3890d1f26f3cdd4b488616d6e95
Ryan Gaudier <113002432+RyanGudi@users.noreply.github.com>
2024-11-02 19:38:38 -0700

    API call link setup

--- jeopardy-game/src/components/GameBoard.vue
+++ jeopardy-game/src/components/GameBoard.vue
@@ -18,27 +18,41 @@
     export default {
         data() {
             return {
-                categories: ['Math','Science','Reading','Music'],
+                categories: ['','','',''],
                 rows: [
                 { scores: ['100', '100', '100', '100'] },
                 { scores: ['200', '200', '200', '200'] },
                 { scores: ['300', '300', '300', '300'] }
                 ],
+                questions: ['','','','',
+                            '','','','',
+                            '','','',''
+                            ]
             };
         },
         methods: {
             async fetchData() {
                 try {
-                    const apiUrl = 'https://opentdb.com/api.php?amount=4&type=boolean'
+                    const categoryNum = Math.floor(Math.random() * 9) + 20;
+                    const apiUrl = `https://opentdb.com/api.php?amount=4&category=${categoryNum}&type=boolean`;
                     const response = await fetch(apiUrl);
+                    console.log(response.results);
+                    const data = await response.json();
+                    console.log(data);
+                    console.log('Category', data.results[0].category);
                     if (!response.ok) {
                         throw new Error('Network response was not ok');
                     }
-                    console.log(response);
+
+                    //this.processResults();
                 } catch(error) {
                     console.error('Error fetching data', error);
                 }
-            }
+            },
+            // processResults(results) {
+            //     //this.categories = Array.from(new Set(results.map(result => result.category)));
+
+            // }
         }
     }
-</script>'
+</script>
